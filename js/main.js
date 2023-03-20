089246a6fd0a0308dedbd734f55d4d1572b3e71b
Fredo St Fleur <fredostfleur23@gmail.com>
2023-03-20 16:59:21 -0400

    -Add showBook fuction

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// import AddNewBook from "./addNewBook.js";
-
+// import { hola as AddNewBook } from "./addNewBook.js";
 let collectionOfBooks = [
   {
     title: 'first book',
@@ -10,21 +9,35 @@ let collectionOfBooks = [
     author: 'name of the author'
   },
 ]
-
+document.addEventListener('DOMContentLoaded', e => {
+  showBooks()
+})
 let fragment = document.createDocumentFragment()
-
-collectionOfBooks.forEach(element => {
-  console.log(element)
-  
-  let div = document.createElement('div')
-  div.innerHTML = `
-  <p class="fw-bold" >${element.title}</p>
-  <p class="fw-bold" >${element.author}</p>
-  <button>Remove</button>
-  <hr class="border border-3 border-dark" >
-  `  
+document.addEventListener('click', e => {
+  e.preventDefault()
+  if (e.target.matches('#submit')) {
+    let title = document.querySelector('#title').value
+    let author = document.querySelector('#author').value
+    let newBook = {
+      title: title,
+      author: author
+    }
+    collectionOfBooks.push(newBook)
+    console.log(collectionOfBooks)
+    showBooks()
+  }
+})
+function showBooks() {
   let container = document.querySelector('.bookListCont')
-  container.appendChild(div)
-
-});
-
+  container.innerHTML = ``
+  collectionOfBooks.forEach(element => {
+    console.log(element)
+    let div = document.createElement('div')
+    div.innerHTML = `
+    <p class="fw-bold" >${element.title}</p>
+    <p class="fw-bold" >${element.author}</p>
+    <button>Remove</button>
+    <hr class="border border-3 border-dark" >`
+    container.appendChild(div)
+  });
+}
