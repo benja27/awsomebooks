268cb448eb6eaa3ac4f9b3a7600bbb113f9ffe8a
benja27 <50673336+benja27@users.noreply.github.com>
2023-03-21 01:29:13 -0600

    Add linter fixes

--- js/addNewBook.js
+++ js/addNewBook.js
@@ -1,12 +1,24 @@
 export default function AddNewBook(e, books) {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
+  
+  if(!title || !author) return alert('please enter a tittle and a author')
+  
   const newBook = {
     title,
     author,
   };
+
+  const isrepeated = books.find(objeto => objeto.title === newBook.title && objeto.author === newBook.author );
+
+  if (isrepeated) {
+    return alert('that book is duplicated')
+  } 
+
   books.push(newBook);
+
   localStorage.setItem('collection', JSON.stringify(books));
 
   return books;
 }
+
--- js/removeBook.js
+++ js/removeBook.js
@@ -1,13 +1,13 @@
-export default function removeBook(e, list) {
-  // const newCollectionOfBooks = [];
-
+export default function removeBook(e, list) {  
   if (e.target.matches('.remove')) {
-    const author = e.srcElement.previousElementSibling.textContent;
-
-    const newCollectionOfBooks = list.filter((book) => book.author !== author);
-
-    localStorage.setItem('collection', JSON.stringify(newCollectionOfBooks));
-    return newCollectionOfBooks;
+    
+    let id = e.srcElement.getAttribute('data-id')
+    
+    let newCollectionOfBooks = list.filter( function (book,index) {       
+      return id != index
+    });
+    list = newCollectionOfBooks    
+    localStorage.setItem('collection', JSON.stringify(list));    
   }
 
   return list;
