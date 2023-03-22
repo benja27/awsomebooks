d8de24e627d8038be4b64d880093c7e2e72367b8
benja27 <50673336+benja27@users.noreply.github.com>
2023-03-22 14:33:11 -0600

    Add Contact info and fix linter errors

--- js/main.js
+++ js/main.js
@@ -69,19 +69,19 @@ if (localStorage.getItem('collection')) {
 
 const navLink = document.querySelectorAll('.nav-link');
 
-navLink[0].addEventListener("click", () => {
+navLink[0].addEventListener('click', () => {
   navLink[0].style.color = 'blue';
-  navLink[1].style.color ='';
+  navLink[1].style.color = '';
   navLink[2].style.color = '';
 });
 
-navLink[1].addEventListener("click", () => {
+navLink[1].addEventListener('click', () => {
   navLink[0].style.color = '';
   navLink[1].style.color = 'blue';
   navLink[2].style.color = '';
 });
 
-navLink[2].addEventListener("click", () => {
+navLink[2].addEventListener('click', () => {
   navLink[0].style.color = '';
   navLink[1].style.color = '';
   navLink[2].style.color = 'blue';
